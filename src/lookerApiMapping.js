666eb909b6b8feb32f0dec2d544b337f3b4b9d4b
ajefts <ajefts@users.noreply.github.com>
2017-05-02 16:23:25 -0400

    added manager role access

--- src/lookerApiMapping.js
+++ src/lookerApiMapping.js
@@ -3,12 +3,12 @@ const USER_ROLE = require('./constants').USER_ROLE;
 module.exports = {
   "runLook": {
     "url": "/looks/{look_id}/run/{result_format}",
-    "roles": [USER_ROLE.TOPCODER_ADMIN],
+    "roles": [USER_ROLE.TOPCODER_ADMIN, USER_ROLE.MANAGER],
     "method": "get"
   },
   "allLooks": {
     "url": "/looks",
-    "roles": [USER_ROLE.TOPCODER_ADMIN],
+    "roles": [USER_ROLE.TOPCODER_ADMIN, USER_ROLE.MANAGER],
     "method": "get"
   }
 }
